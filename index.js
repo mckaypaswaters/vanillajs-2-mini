f747c11de6cc58e2f6a8855be9cfea73c53c8b5f
McKay Waters <mckaypaswaters@gmail.com>
2019-08-21 13:59:04 -0600

    finished all but black diamond

---
+++ index.js
@@ -0,0 +1,31 @@
+document.querySelector('form').addEventListener('submit', addTodo)
+
+
+function addTodo(event){
+    event.preventDefault()
+    const item = document.createElement('li')
+    const input = document.querySelector('#item')
+    item.innerText = input.value
+    item.addEventListener('click', completeTodo)
+
+    const button = document.createElement('button')
+    button.innerText = 'X'
+    button.addEventListener('click', removeTodo)
+    item.append(button)
+
+    const list = document.querySelector('ul')
+    list.appendChild(item)
+}
+
+function removeTodo(event) {
+    event.target.parentNode.remove()
+}
+
+function completeTodo(event) {
+    if (event.target.getAttribute('aria-checked') !== 'true') {
+        event.target.setAttribute('aria-checked', 'true')
+    } else {
+        event.target.setAttribute('aria-checked', 'false')
+    }
+
+}
